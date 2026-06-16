e6086601f36826fc3393a26943f725edf5ee4a83
Faiztried221 <faizgaia02@gmail.com>
2026-06-16 14:37:39 +0700

    Menambahkan stocks.cpp dan stock.hpp dan membuat menu tester di main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include "model.hpp"
 #include "cart.hpp"
 #include "transaction.hpp"
+#include "stocks.hpp"
 
 int main() {
     Product products[100];
     int productCount = 2;
-
+    int pilih;
+    
     products[0] = Product{"BR001", "Indomie", 3500, 50};
     products[1] = Product{"BR002", "Aqua", 4000, 30};
 
@@ -17,6 +19,14 @@ int main() {
 
     string cashierId = "KS001";
 
+	do{ //ubah sesuai kebutuhan atau kalau mau testing
+		cout << "Test menu\n";
+		cout << "1. Transaction\n";
+		cout << "2. Manage Stock\n";
+		cout << "Select menu : ";
+		cin >> pilih;
+		switch (pilih){
+			case 1:
     startTransaction(
         products,
         productCount,
@@ -25,6 +35,10 @@ int main() {
         &cash,
         cashierId
     );
+			case 2:
+			Stokmenu(products, productCount);				
+		}
+	}while (pilih != 3);
 
     return 0;
 }
